fa372dc21554b5356d89050bd6af98e46bbe6c33
Aamina Bibi <aaminabibi2324@gmail.com>
2024-06-22 00:09:07 +0500

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const express=require  ('express')
+const app=express()
+const port=5000;
+app.get('/',(req,res)=>{
+    res.send('hello world.......')
+})
+
+app.listen(port,console.log(`app is running on port ${port}`))
